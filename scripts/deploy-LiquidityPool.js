5f6d3bd10c31380b9d1482aa2090d3e2c24d4761
johnfr14 <jonathan.alyra@gmail.com>
2021-08-10 11:00:42 +0200

    scripts

--- scripts/deploy-LiquidityPool.js
+++ scripts/deploy-LiquidityPool.js
@@ -16,15 +16,15 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address);
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
+  const Pool = await hre.ethers.getContractFactory('LiquidityPool');
+  const pool = await Pool.deploy("addre Token0", "address Token1");
 
   // Attendre que le contrat soit réellement déployé, cad que la transaction de déploiement
   // soit incluse dans un bloc
-  await greeter.deployed();
+  await pool.deployed();
 
   // Create/update deployed.json and print usefull information on the console.
-  await deployed('Greeter', hre.network.name, greeter.address);
+  await deployed('LiquidityPool', hre.network.name, pool.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
